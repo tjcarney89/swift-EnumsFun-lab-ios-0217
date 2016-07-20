3a0774b2ef20411540a81fb64dab68e7b3ec9e0f
JimCampagno <jimcampagno@gmail.com>
2016-07-20 14:42:52 -0400

    Add Quick & Nimble

---
+++ Pods/Target Support Files/Pods-EnumsFunTests/Pods-EnumsFunTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EnumsFunTests : NSObject
+@end
+@implementation PodsDummy_Pods_EnumsFunTests
+@end
---
+++ Pods/Target Support Files/Pods-EnumsFunTests/Pods-EnumsFunTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_EnumsFunTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EnumsFunTestsVersionString[];
+
