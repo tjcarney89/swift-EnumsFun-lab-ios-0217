c3abc8a7a357ee18565eda5f64580d30c3d0cccb
Michael Dippery <michael@monkey-robot.com>
2016-08-02 19:38:47 -0700

    Use enums with raw values
    Students haven't explicitly been taught these yet, but I'll mention it
    to them in the lab materials.

--- EnumsFun/Card.swift
+++ EnumsFun/Card.swift
@@ -8,59 +8,20 @@
 
 import Foundation
 
-enum Suit {
-    case Spades, Diamonds, Hearts, Clubs
-    var symbol: String {
-        switch self {
-        case .Spades:
-            return "♠️"
-        case .Diamonds:
-            return "♦️"
-        case .Hearts:
-            return "♥️"
-        case .Clubs:
-            return "♣️"
-        }
-    }
+enum Suit: String {
+    case Spades = "♠️", Diamonds = "♦️", Hearts = "♥️", Clubs = "♣️"
 }
 
-enum Rank {
-    case Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two
-    var value: Int {
-        switch self {
-        case .Ace:
-            return 13
-        case .King:
-            return 12
-        case .Queen:
-            return 11
-        case .Jack:
-            return 10
-        case .Ten:
-            return 9
-        case .Nine:
-            return 8
-        case .Eight:
-            return 7
-        case .Seven:
-            return 6
-        case .Six:
-            return 5
-        case .Five:
-            return 4
-        case .Four:
-            return 3
-        case .Three:
-            return 2
-        case .Two:
-            return 1
-        }
-    }
+enum Rank: Int {
+    case One = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, King, Queen, Ace
 }
 
 class Card {
     let suit: Suit
     let rank: Rank
+    var description: String {
+        return "\(rank.rawValue)\(suit.rawValue)"
+    }
 
     init(rank: Rank, suit: Suit) {
         self.rank = rank
